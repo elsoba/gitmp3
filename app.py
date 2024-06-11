f306c0e7a060ca1c9f7c69292b6388e89bb95cd2
elsoba <84707822+elsoba@users.noreply.github.com>
2024-06-11 19:17:08 -0300

    Update app.py

--- app.py
+++ app.py
@@ -207,7 +207,7 @@ def download():
                 return redirect(url_for('index'))
 
     return redirect(url_for('index'))
-    
+
 if __name__ == "__main__":
     port = int(os.environ.get('PORT'))
     app.run(host='0.0.0.0', port=port)
